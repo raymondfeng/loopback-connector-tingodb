4cc81de391fb3afb56cbfebdfb1db7ff88038089
Raymond Feng <raymond@strongloop.com>
2013-10-12 10:20:18 -0700

    Update example

--- example/app.js
+++ example/app.js
@@ -1,21 +1,21 @@
-var Schema = require('jugglingdb').Schema;
+var DataSource = require('loopback-datasource-juggler').DataSource;
 var path = require('path');
 
-
-var db = new Schema(require('../'), {
+var db = new DataSource(require('../'), {
     path: path.join(__dirname, 'tingodb'),
     // inMemoryOnly: true,
     debug: true
 });
 
-User = db.define('User1', {
-    name:      { type: String, index: true },
-    email:     { type: String, index: true },
-    age:          Number,
-    notes: {type: []}
+User = db.createModel('User1', {
+    name: { type: String, index: true },
+    email: { type: String, index: true },
+    age: Number,
+    notes: {type: [String]}
 });
 
 User.create({name: 'Ray', email: 'x@y.com', notes: ['A']}, function(err, result) {
+    console.log(err, result);
     User.findById(result.id, console.log);
     User.findOne(function(e, u) {
         User.findOne({where: {
